1230706e51bba5126582079d780876ffb5810d7d
Tigro <ashejn@gmail.com>
2011-12-15 14:08:05 +0400

    another url

--- get_sources.sh
+++ get_sources.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -sO http://downloads.sourceforge.net/project/liferea/Liferea%20Stable/1.8.0/liferea-1.8.0.tar.gz
+curl -sO http://ignum.dl.sourceforge.net/project/liferea/Liferea%20Stable/1.8.0/liferea-1.8.0.tar.gz
 
